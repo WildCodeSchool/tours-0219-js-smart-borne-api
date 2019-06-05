c97123b90b3a2407a904af074f101d2e2a0d9330
mdhn <maxduchan@gmail.com>
2019-06-05 17:41:36 +0200

    remove dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BorneModule } from './borne/borne.module';
-require('dotenv').config();
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  require('dotenv').config();
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api');
